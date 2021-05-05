49938703b84dfdeaf944b504c471eb6e0a12073d
Tim Swierzewski <TJswierzewski@gmail.com>
2021-05-05 18:21:56 -0400

    case create testing

--- spec/controllers/api/v1/cases_controller_spec.rb
+++ spec/controllers/api/v1/cases_controller_spec.rb
@@ -18,4 +18,42 @@ RSpec.describe Api::V1::CasesController, type: :controller do
       expect(returned_json[0]['width']).to be_falsey
     end
   end
+
+  describe 'POST#create' do
+    post_json = { prefix: 'MA', case_number: 700, height: 48, length: 30, width: 30, weight: 170 }
+    post_error = { prefix: 'MA' }
+
+    it 'creates a new case' do
+      prev_count = Case.count
+      post(:create, params: post_json, format: :json)
+      expect(Case.count).to eq(prev_count + 1)
+    end
+
+    it 'returns a persisted case' do
+      post(:create, params: post_json, format: :json)
+      returned_json = JSON.parse(response.body)
+
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq('application/json')
+
+      expect(returned_json).to be_kind_of(Hash)
+      expect(returned_json['id']).to be_truthy
+      expect(returned_json['prefix']).to eq 'MA'
+      expect(returned_json['case_number']).to eq 700
+    end
+
+    it 'does not create a case when incorrect data is recieved.' do
+      post(:create, params: post_error, format: :json)
+      returned_json = JSON.parse(response.body)
+
+      expect(response.status).to eq 422
+      expect(response.content_type).to eq('application/json')
+
+      expect(returned_json['errors']['case_number'].first).to eq "Case number can't be blank"
+      expect(returned_json['errors']['height'].first).to eq "Height can't be blank"
+      expect(returned_json['errors']['width'].first).to eq "Width can't be blank"
+      expect(returned_json['errors']['length'].first).to eq "Length can't be blank"
+      expect(returned_json['errors']['weight'].first).to eq "Weight can't be blank"
+    end
+  end
 end
