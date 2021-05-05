5c25898f0ce48d2d36a6dc1d4a0f872da3855eef
Tim Swierzewski <TJswierzewski@gmail.com>
2021-05-05 15:48:41 -0400

    Case Form not styled

--- app/javascript/react/components/CaseForm.js
+++ app/javascript/react/components/CaseForm.js
@@ -1,77 +1,83 @@
-import { Button, makeStyles, TextField } from '@material-ui/core';
-import { FormikConsumer, useFormik } from 'formik';
+import { makeStyles } from '@material-ui/core';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
 import React from 'react';
 import * as Yup from 'yup';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    margin: 5
+const useStyles = makeStyles(() => ({
+  error: {
+    '& p': {
+      color: 'red'
+    }
   }
 }));
-const classes = useStyles;
-
 const CaseForm = () => {
-  const formik = useFormik({
-    initialValues: {
-      prefix: '',
-      case_number: '',
-      height: '',
-      length: '',
-      width: '',
-      weight: ''
-    },
-    validationSchema: Yup.object({
-      prefix: Yup.string()
-        .required('Required')
-        .matches(/[A-Z]{2}/, 'Please use 2 capital letters'),
-      case_number: Yup.number()
-        .required('Required')
-        .integer('Please use a integer')
-        .lessThan(1000, 'Use a 3 digit number'),
-      height: Yup.number().required('Required'),
-      length: Yup.number().required('Required'),
-      width: Yup.number().required('Required'),
-      weight: Yup.number().required('Required')
-    }),
-    onSubmit: (values) => {
-      console.log(values);
-    }
-  });
+  const classes = useStyles();
   return (
-    <div>
-      <form className={classes.root} onSubmit={formik.handleSubmit}>
-        <TextField
-          error={Boolean(formik.errors.prefix)}
-          color="secondary"
-          variant="standard"
-          fullWidth
-          id="prefix"
-          name="prefix"
-          label="Prefix"
-          value={formik.values.prefix}
-          error={formik.touched.prefix && Boolean(formik.errors.prefix)}
-          helperText={
-            formik.touched.prefix && formik.errors.prefix ? FormikConsumer.errors.prefix : ''
-          }
-        />
-        <TextField
-          color="secondary"
-          error={Boolean(formik.errors.case_number)}
-          variant="outlined"
-          fullWidth
-          id="case_number"
-          name="case_number"
-          label="Case Number"
-          value={formik.values.case_number}
-          onChange={formik.handleChange}
-          error={formik.touched.case_number && Boolean(formik.errors.case_number)}
-          helperText={formik.touched.case_number && formik.errors.case_number}
-        />
-        <Button color="primary" variant="contained" fullWidth type="submit">
-          Submit
-        </Button>
-      </form>
-    </div>
+    <Formik
+      initialValues={{
+        prefix: '',
+        case_number: '',
+        height: '',
+        length: '',
+        width: '',
+        weight: ''
+      }}
+      validationSchema={Yup.object({
+        prefix: Yup.string()
+          .required('Required')
+          .matches(/[A-Z]{2}/, 'Please use 2 capital letters'),
+        case_number: Yup.number()
+          .required('Required')
+          .integer('Please use a integer')
+          .lessThan(1000, 'Use a 3 digit number'),
+        height: Yup.number().required('Required'),
+        length: Yup.number().required('Required'),
+        width: Yup.number().required('Required'),
+        weight: Yup.number().required('Required')
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      <Form>
+        <div className={classes.error}>
+          <label htmlFor="prefix">Prefix</label>
+          <Field name="prefix" type="text" />
+          <ErrorMessage component="p" name="prefix" />
+        </div>
+        <div>
+          <label htmlFor="case_number">Case Number</label>
+          <Field name="case_number" type="text" />
+          <ErrorMessage name="case_number" />
+        </div>
+        <div>
+          <label htmlFor="height">Height</label>
+          <Field name="height" type="text" />
+          <ErrorMessage name="height" />
+        </div>
+        <div>
+          <label htmlFor="length">Length</label>
+          <Field name="length" type="text" />
+          <ErrorMessage name="length" />
+        </div>
+        <div>
+          <label htmlFor="width">Width</label>
+          <Field name="width" type="text" />
+          <ErrorMessage name="width" />
+        </div>
+        <div>
+          <label htmlFor="weight">Weight</label>
+          <Field name="weight" type="text" />
+          <ErrorMessage name="weight" />
+        </div>
+        <br />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
   );
 };
 
