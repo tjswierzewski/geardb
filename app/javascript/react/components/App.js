773e34e8804ec563bca89c9ed911b4217f662cd7
Tim Swierzewski <TJswierzewski@gmail.com>
2021-05-06 23:45:18 -0400

    draggable config

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -5,20 +5,24 @@ import { Grid } from '@material-ui/core';
 import CaseContainer from './case/CaseContainer';
 import GearContainer from './Electronic/GearContainer';
 import Layout from './Layout';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Layout>
-        <Grid container>
-          <Grid item xs={6}>
-            <CaseContainer />
+      <DndProvider backend={HTML5Backend}>
+        <Layout>
+          <Grid container>
+            <Grid item xs={6}>
+              <CaseContainer />
+            </Grid>
+            <Grid item xs={6}>
+              <GearContainer />
+            </Grid>
           </Grid>
-          <Grid item xs={6}>
-            <GearContainer />
-          </Grid>
-        </Grid>
-      </Layout>
+        </Layout>
+      </DndProvider>
     </ThemeProvider>
   );
 };
---
+++ app/javascript/react/constants/ItemTypes.js
@@ -0,0 +1,3 @@
+const ItemTypes = {
+  ELECTRONIC = 'electronic'
+}
