9d7d4133179493244478fd2688742e5210ca56bc
Tim Swierzewski <TJswierzewski@gmail.com>
2021-05-07 22:57:34 -0400

    Faker issue

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -11,11 +11,11 @@ FactoryBot.define do
     name { Faker::Appliance.equipment }
     manufacture { Faker::Appliance.brand }
     model_number { Faker::Barcode.ean }
-    serial_number { Faker::Device.unique.serial }
+    serial_number { Faker::Device.serial }
     cost { Faker::Number.number(digits: 3) }
     firmware_version { Faker::Internet.ip_v4_address }
     software_version { Faker::Computer.os }
-    barcode { Faker::Barcode.unique.ean }
+    barcode { Faker::Barcode.ean }
   end
 
   factory :case do
