291d4c1e5c11558fe7740bdcab6381a6ab800062
Tim Swierzewski <TJswierzewski@gmail.com>
2021-05-12 12:45:37 -0400

    tour inventory selector api

--- spec/controllers/api/v1/tours_controller_spec.rb
+++ spec/controllers/api/v1/tours_controller_spec.rb
@@ -79,4 +79,33 @@ RSpec.describe Api::V1::ToursController, type: :controller do
       expect(returned_json['cases'][0]['weight']).to be_falsey
     end
   end
+
+  describe 'GET#items' do
+    let!(:tour1) { Tour.first }
+    let!(:case1) { FactoryBot.create(:case) }
+    let!(:case2) { FactoryBot.create(:case) }
+    let!(:case3) { FactoryBot.create(:case) }
+    let!(:assignment1) { CaseAssignment.create(case: case1, tour: tour1) }
+    let!(:assignment2) { CaseAssignment.create(case: case2, tour: tour1) }
+    let!(:assignment3) { CaseAssignment.create(case: case3, tour: tour1) }
+    let!(:electronics1) { FactoryBot.create(:electronic, case: case1) }
+    let!(:electronics2) { FactoryBot.create(:electronic, case: case1) }
+    let!(:electronics3) { FactoryBot.create(:electronic, case: case1) }
+    let!(:electronics4) { FactoryBot.create(:electronic, case: case2) }
+    let!(:electronics5) { FactoryBot.create(:electronic, case: case3) }
+    it 'gets the electronics that are in a case' do
+      get :items, params: { id: tour1.id }
+      returned_json = JSON.parse(response.body)
+
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq('application/json')
+
+      expect(returned_json['electronics'].length).to eq 5
+      expect(returned_json['electronics'][0]['name']).to be_truthy
+      expect(returned_json['electronics'][0]['model_number']).to be_truthy
+      expect(returned_json['electronics'][0]['barcode']).to be_truthy
+      expect(returned_json['electronics'][0]['serial_number']).to be_falsey
+      expect(returned_json['electronics'][0]['cost']).to be_falsey
+    end
+  end
 end
