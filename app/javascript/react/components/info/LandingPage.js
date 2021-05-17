6d30d2e05288dd5ac3b1aaf0f8ded3b767134032
Tim Swierzewski <TJswierzewski@gmail.com>
2021-05-17 12:42:38 -0400

    style landing page

--- app/javascript/react/components/info/LandingPage.js
+++ app/javascript/react/components/info/LandingPage.js
@@ -1,21 +1,33 @@
 import { makeStyles, Paper, Typography } from '@material-ui/core';
 import React from 'react';
 
-const useStyles = makeStyles({});
+const useStyles = makeStyles({
+  root: {
+    margin: '2rem auto',
+    maxWidth: '1400px'
+  },
+  usage: {
+    marginTop: '1.5rem'
+  },
+  description: {
+    maxWidth: '800px',
+    margin: '1.5rem auto'
+  }
+});
 const LandingPage = () => {
   const classes = useStyles();
   return (
-    <Paper elevation="3">
+    <Paper className={classes.root} elevation="3">
       <Typography variant="h1" align="center">
         Welcome to Geardb
       </Typography>
       <Typography variant="subtitle1" align="center">
         An inventory managment app for rental companies in the concert industry
       </Typography>
-      <Typography variant="h3" align="center">
+      <Typography className={classes.usage} variant="h3" align="center">
         Usage
       </Typography>
-      <Typography align="center">
+      <Typography className={classes.description} align="center">
         Log in to view your inventory. Once logged in items can be added to three categories:
         Inventory, Cases and Tours. Iventory items can be placed into cases and cases can be placed
         into tours with the drag and drop feature. Tours and cases can be selected to see what other
